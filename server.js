acfacbba07bc0854c80d46b3eefaa63d06b68381
Simon Garber <Simon.garber@gmail.com>
2019-10-03 00:22:05 -0400

    added create user functionality with log out feature utilizing the useState hook for holding the user email

--- server.js
+++ server.js
@@ -12,29 +12,63 @@ let pool = new pg.Pool({
   max: 10
 });
 
-pool.connect((err, db, done) => {
-  if (err) {
-    return console.log(err);
-  } else {
-    db.query('SELECT * from users', (err, table) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(table);
-      }
-    });
-  }
-});
+// pool.connect((err, db, done) => {
+//   if (err) {
+//     return console.log(err);
+//   } else {
+//     let user_email = 'simon.garber@gmail.com';
+//     let user_password_digest = 'password';
+//     let id = Math.random().toFixed(3);
+//     db.query(
+//       'INSERT INTO users (user_email, user_password_digest, id) VALUES($1, $2, $3)',
+//       [user_email, user_password_digest, id],
+//       (err, table) => {
+//         done();
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log('DB insert successful');
+//         }
+//       }
+//     );
+//   }
+// });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
+app.use(function(request, response, next) {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
   next();
+});
+
+app.post('/api/new-user', (request, response) => {
+  const user_email = request.body.email;
+  const user_password_digest = request.body.password;
+  var id = Math.random().toFixed(3);
+  let values = [user_email, user_password_digest, id];
+  pool.connect((err, db, done) => {
+    if (err) {
+      return response.status(400).send(err);
+    } else {
+      db.query(
+        'INSERT INTO users (user_email, user_password_digest, id) VALUES($1, $2, $3)',
+        [...values],
+        (err, table) => {
+          done();
+          if (err) {
+            console.log(err);
+          } else {
+            response.status(201).send(request.body);
+          }
+        }
+      );
+    }
+  });
 });
 
 app.listen(PORT, () => console.log('listening on port ' + PORT));
