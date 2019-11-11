637d4a21ba62e009bb597651919f937a2416ff40
Simon Garber <Simon.garber@gmail.com>
2019-11-11 05:07:38 -0500

    added a dynamic query builder with 4 input fields that can be used independently or in conjunction to perform a search

--- queries.js
+++ queries.js
@@ -1,10 +1,6 @@
 const { Pool } = require('pg');
 const mongodb = require('mongodb');
 const queryString = require('querystring');
-const url = require('url');
-let rawUrl = '/api/cards/:card';
-let parsedUrl = url.parse(rawUrl);
-let parsedQs = queryString.parse(parsedUrl.query);
 
 const pool = new Pool({
   port: 5432,
@@ -95,131 +91,45 @@ const saveCard = (request, response) => {
 };
 // Query that runs a search against the mtgcards db based on a match with any text in any key within the card object
 const getCards = async (request, response) => {
-  console.log(request.query);
   const cards = await loadCardsCollection();
-  const set = request.query.set.length > 0 ? request.query.set : null;
-  const name = request.query.name.length > 0 ? request.query.name : null;
-  const cmc = request.query.cmc.length > 0 ? request.query.cmc : null;
-  const typeLine =
-    request.query.typeLine.length > 0 ? request.query.typeLine : null;
-  if (name && set == null && cmc == null && typeLine == null) {
-    response.send(
-      await cards
-        .find({
-          name: { $regex: `.*${name}.*` }
-        })
-        .toArray()
-    );
-  } else if (set && name == null && cmc == null && typeLine == null) {
-    response.send(
-      await cards
-        .find({
-          set: `${set}`
-        })
-        .toArray()
-    );
-  } else if (cmc && name == null && typeLine == null && set == null) {
-    response.send(
-      await cards
-        .find({
-          cmc: parseInt(`${cmc}`)
-        })
-        .toArray()
-    );
-  } else if (typeLine && cmc == null && name == null && set == null) {
-    response.send(
-      await cards
-        .find({
-          type_line: { $regex: `.*${typeLine}.*` }
-        })
-        .toArray()
-    );
-  } else if (name && set && cmc == null && typeLine == null) {
-    response.send(
-      await cards
-        .find({
-          name: { $regex: `.*${name}.*` },
 
-          set: `${set}`
-        })
-        .toArray()
-    );
-  } else if (set && typeLine && cmc && name == null) {
-    response.send(
-      await cards
-        .find({
-          set: `${set}`,
-          type_line: { $regex: `.*${typeLine}.*` },
-          cmc: parseInt(`${cmc}`)
-        })
-        .toArray()
-    );
-  } else if (typeLine && cmc && name == null && set == null) {
-    response.send(
-      await cards
-        .find({
-          type_line: { $regex: `.*${typeLine}.*` },
-          cmc: parseInt(`${cmc}`)
-        })
-        .toArray()
-    );
-  } else if (typeLine && set && name == null && cmc == null) {
-    response.send(
-      await cards
-        .find({
-          type_line: { $regex: `.*${typeLine}.*` },
-          set: `${set}`
-        })
-        .toArray()
-    );
-  } else if (name && cmc && set == null && typeLine == null) {
-    response.send(
-      await cards
-        .find({
-          name: { $regex: `.*${name}.*` },
-          cmc: parseInt(`${cmc}`)
-        })
-        .toArray()
-    );
-  } else {
-    response.send('Please try again');
+  const { name, set, cmc, typeLine, oracleText } = request.query;
+  const queryObj = {};
+  if (name !== '') {
+    queryObj['name'] = { $regex: `.*${name}.*` };
   }
-  console.log(name);
-  console.log(set);
-  console.log(cmc);
-  console.log(typeLine);
-  console.log(typeof cmc);
-};
+  if (set !== '') {
+    queryObj['set'] = `${set}`;
+  }
+  if (cmc !== '') {
+    queryObj['cmc'] = parseInt(cmc);
+  }
+  if (typeLine !== '') {
+    queryObj['type_line'] = { $regex: `.*${typeLine}.*` };
+  }
+  if (oracleText !== '') {
+    queryObj['oracle_text'] = { $regex: `.*${oracleText}.*` };
+  }
 
-async function loadCardsCollection() {
-  const client = await mongodb.MongoClient.connect(
-    'mongodb+srv://admin:admin1234@cluster0-dposj.mongodb.net/test?retryWrites=true&w=majority',
-    {
-      useUnifiedTopology: true
-    }
+  response.send(
+    await cards
+      .find({
+        ...queryObj
+      })
+      .toArray()
   );
-  return client.db('mtgcards').collection('data');
-}
-// const getCards = (request, response) => {
-//   const card = request.params.card;
-//   pool.connect((err, db) => {
-//     if (err) {
-//       throw err;
-//     } else {
-//       db.query(
-//         `
-//         SELECT * FROM mtgcards WHERE lower(data::text) like lower('%${card}%');`,
-//         (error, table) => {
-//           if (error) {
-//             throw error;
-//           } else {
-//             return response.json(table.rows);
-//           }
-//         }
-//       );
-//     }
-//   });
-// };
+
+  async function loadCardsCollection() {
+    const client = await mongodb.MongoClient.connect(
+      'mongodb+srv://admin:admin1234@cluster0-dposj.mongodb.net/test?retryWrites=true&w=majority',
+      {
+        useUnifiedTopology: true,
+        ignoreUndefined: true
+      }
+    );
+    return client.db('mtgcards').collection('data');
+  }
+};
 
 const setSearch = (request, response) => {
   const set = request.params.set;
