149ae325b22d8db330647734e9a27178a0442b5b
Simon Garber <Simon.garber@gmail.com>
2019-11-12 23:05:35 -0500

    added color identity query

--- queries.js
+++ queries.js
@@ -3,7 +3,7 @@ const mongodb = require('mongodb');
 const getCards = async (request, response) => {
   const cards = await loadCardsCollection();
 
-  const { name, set, cmc, typeLine, oracleText } = request.query;
+  const { name, set, cmc, typeLine, oracleText, colorIdentity } = request.query;
   const queryObj = {};
   if (name !== '') {
     queryObj['name'] = { $regex: `.*${name}.*` };
@@ -19,6 +19,10 @@ const getCards = async (request, response) => {
   }
   if (oracleText !== '') {
     queryObj['oracle_text'] = { $regex: `.*${oracleText}.*` };
+  }
+  // Query that checks if the Array of color identities contains the value passed in the form
+  if (colorIdentity !== '') {
+    queryObj['color_identity'] = `${colorIdentity}`;
   }
 
   response.send(
