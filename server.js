d5a770b2303f808acf237827c7a9272d3591fcf6
Simon Garber <Simon.garber@gmail.com>
2019-11-14 15:25:02 -0500

    pre deployment check

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 3001;
 const app = express();
-const db = require('./queries');
+const db = require("./queries");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(request, response, next) {
-  response.header('Access-Control-Allow-Origin', '*');
+  response.header("Access-Control-Allow-Origin", "*");
   response.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
 // Root
-app.get('/', (request, response) => {
-  response.json({ info: 'node.js, express, mongodb' });
+app.get("/", (request, response) => {
+  response.json({ info: "node.js, express, mongodb" });
 });
 
-app.get('/api/cards/', db.getCards);
+app.get("/api/cards/", db.getCards);
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, () => console.log("listening on port", port));
