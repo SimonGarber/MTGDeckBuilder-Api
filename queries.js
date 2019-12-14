76970c25f32639c537e8270c2a2a885a5c47ecc5
Simon Garber <Simon.garber@gmail.com>
2019-12-14 17:36:31 -0500

    updated admin password for mongodb

--- queries.js
+++ queries.js
@@ -1,28 +1,28 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const getCards = async (request, response) => {
   const cards = await loadCardsCollection();
 
   const { name, set, cmc, typeLine, oracleText, colorIdentity } = request.query;
   const queryObj = {};
-  if (name !== '') {
-    queryObj['name'] = { $regex: `.*${name}.*` };
+  if (name !== "") {
+    queryObj["name"] = { $regex: `.*${name}.*` };
   }
-  if (set !== '') {
-    queryObj['set'] = `${set}`;
+  if (set !== "") {
+    queryObj["set"] = `${set}`;
   }
-  if (cmc !== '') {
-    queryObj['cmc'] = parseInt(cmc);
+  if (cmc !== "") {
+    queryObj["cmc"] = parseInt(cmc);
   }
-  if (typeLine !== '') {
-    queryObj['type_line'] = { $regex: `.*${typeLine}.*` };
+  if (typeLine !== "") {
+    queryObj["type_line"] = { $regex: `.*${typeLine}.*` };
   }
-  if (oracleText !== '') {
-    queryObj['oracle_text'] = { $regex: `.*${oracleText}.*` };
+  if (oracleText !== "") {
+    queryObj["oracle_text"] = { $regex: `.*${oracleText}.*` };
   }
   // Query that checks if the Array of color identities contains the value passed in the form
-  if (colorIdentity !== '') {
-    queryObj['color_identity'] = `${colorIdentity}`;
+  if (colorIdentity !== "") {
+    queryObj["color_identity"] = `${colorIdentity}`;
   }
 
   response.send(
@@ -35,13 +35,13 @@ const getCards = async (request, response) => {
 
   async function loadCardsCollection() {
     const client = await mongodb.MongoClient.connect(
-      'mongodb+srv://admin:admin1234@cluster0-dposj.mongodb.net/test?retryWrites=true&w=majority',
+      "mongodb+srv://admin:Dpb56MGY?8+ly11@cluster0-dposj.mongodb.net/test?retryWrites=true&w=majority",
       {
         useUnifiedTopology: true,
         ignoreUndefined: true
       }
     );
-    return client.db('mtgcards').collection('data');
+    return client.db("mtgcards").collection("data");
   }
 };
 
